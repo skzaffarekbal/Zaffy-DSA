150e2c1f160c086f8f76c6f3f34e41724e185772
Sk Zaffar Ekbal <s.zaffarekbal@gmail.com>
2025-01-01 22:40:23 +0530

    Two Sum Solve in JS

---
+++ Array/1_Two-Sum.js
@@ -0,0 +1,76 @@
+/* Date: 01/01/2025 */ 
+
+/*
+Link : https://leetcode.com/problems/two-sum/description/
+*/
+
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+*/
+
+/*
+First Solution
+Time complexity - O(n2)
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let numLength = nums.length;
+    for(let i = 0; i < numLength; i++){
+        for(let j = i + 1; j < numLength; j++){
+            if(nums[i] + nums[j] === target) return [i, j];
+        }
+    }
+    return [-1, -1];
+};
+
+/*
+Second Solution
+Time complexity - O(n)
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let memory = {};
+    let numLength = nums.length;
+    for(let i = 0; i < numLength; i++){
+        let wantting = target - nums[i];
+        if(memory.hasOwnProperty(wantting)) return [memory[wantting], i]
+        else memory[nums[i]] = i;
+    }
+};
